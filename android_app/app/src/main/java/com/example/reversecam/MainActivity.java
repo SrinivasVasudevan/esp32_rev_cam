5d3d5542810fdf7d8bae2924c6c56605fe498a93
SrinivasVasudevan <srinivasvasudevan2000@gmail.com>
2026-02-24 19:27:32 -0500

    Add android app base files

---
+++ android_app/app/src/main/java/com/example/reversecam/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.reversecam;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    // Replace this with the IP address of your ESP32 Access Point
+    private static final String CAMERA_URL = "http://192.168.1.100/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(CAMERA_URL);
+    }
+}
